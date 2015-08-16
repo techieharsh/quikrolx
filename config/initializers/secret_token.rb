312ff792610ea44675d048582f42c7fc900a00dd
techieharsh <techieharsh>
2015-08-16 23:38:39 +0530

    Project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quikr::Application.config.secret_token = 'b873925da02db165b72db13259c14f0d67aea305967bc32c0f6b562276346c556050c39f198d2e9dbcfbda331711ddc2a4087d096146e3848439a86298939b11'
